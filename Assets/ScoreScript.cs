c8782e4f557cb64bfced27501b228946b8fcc06c
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-11-06 17:29:00 +0900

    スコアシーンを代理実装

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    public int score = 0;
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "SCORE:" + score.ToString();
+	}
+}
